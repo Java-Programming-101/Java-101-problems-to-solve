19d4df9de1911b1adf32f5525df2a87adb7fd952
Java Programming 101 <henrywright88404@gmail.com>
2016-04-25 11:25:16 +1200

    4 Challenges added to begin with.

---
+++ src/Problems/Guess_The_Number.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Problems;
+
+/**
+ * The task is to write a program where the program chooses a number between 1 and 10. 
+ * A player is then prompted to enter a guess. If the player guess wrong then the prompt 
+ * appears again until the guess is correct. When the player has made a successful guess 
+ * the computer will give a "Well guessed!" message, and the program will exit.
+ * 
+ * 
+ * Extra Credit - Provide the user with a message after a guess telling them if their guess 
+ * was higher than or lower than the target number.
+ */
+public class Guess_The_Number {
+
+}
---
+++ src/Problems/Nth_Number.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package Problems;
+
+/**
+ * Write a function/method/subroutine/... that when given an integer greater than 
+ * or equal to zero returns a string of the number followed by an apostrophe then 
+ * the ordinal suffix. 
+ * 
+ * Example returns would include 1'st 2'nd 3'rd 11'th 111'th 1001'st 1012'th
+ *
+ */
+public class Nth_Number {
+
+}
